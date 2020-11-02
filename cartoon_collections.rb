824e6a1621e3dbea60695bdb18f5770d045ea88b
Rachel Cross <racross1@gmail.com>
2020-11-02 00:16:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 def list_dwarves(array)
   array.each_with_index do |item, index|
     index_num = index + 1 
-    formatted_index_num = index_num.to_str + ". " 
+    formatted_index_num = index_num.to_s + ". " 
     print formatted_index_num + item
   end
   # Use `each_with_index` to enumerate over the provided array
